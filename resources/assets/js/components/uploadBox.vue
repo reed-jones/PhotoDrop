f440c85ef07f328453a7c1ef77c8114a1323f4ae
Reed Jones <reedjones@reedjones.com>
2018-01-18 20:47:13 -0700

    updated deps

--- resources/assets/js/components/uploadBox.vue
+++ resources/assets/js/components/uploadBox.vue
@@ -1,14 +1,27 @@
 <template>
-  <dropzone id="myVueDropzone" url="/api/media/store" @vdropzone-success="showSuccess" @vdropzone-removed-file="removeFile" :max-file-size-in-m-b='15000' :max-number-of-files='200' :accepted-file-types="'image/*,video/*'" :show-remove-link="false">
+  <dropzone id="myVueDropzone" url="/api/media/store" @vdropzone-success="showSuccess" @vdropzone-removed-file="removeFile" :options="dropzoneOptions">
   </dropzone>
 </template>
 
 <script>
 import Dropzone from 'vue2-dropzone'
+import 'vue2-dropzone/dist/vue2Dropzone.css'
 
 export default {
   components: {
     Dropzone
+  },
+  data() {
+    return {
+      dropzoneOptions: {
+        url: '/api/media/store',
+        showRemoveLink: false,
+        thumbnailWidth: 150,
+        maxFilesize: 15000,
+        maxNumberOfFiles: 200,
+        acceptedFileTypes:'image/*,video/*'
+      }
+    }
   },
   methods: {
     'showSuccess': function(file) {
